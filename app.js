feb82bec2b557213442181db9d7e1e0d2b02a09e
James Brett <jmsbrett@gmail.com>
2018-06-15 17:26:14 -0500

    changes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const auth = require('./routes/auth');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('it works')
-})
+});
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://jmsbrett:Bluesea3483@ds261430.mlab.com:61430/storybooks-dev',
+    googleClientID: '434108093047-jt7avf3klue508vbdggponnc28t4kval.apps.googleusercontent.com',
+    googleClientSecret: 'TEqNxb4Mr2Lz_vEc8iibvWlr'
+}
