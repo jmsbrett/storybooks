5804f9c6324eb6f1ca45de8addd0462d21a905ec
James Brett <jmsbrett@gmail.com>
2018-06-15 17:36:26 -0500

    changes

--- config/passport.js
+++ config/passport.js
@@ -3,5 +3,15 @@ const mongoose = require('mongoose');
 const keys = require('./keys');
 
 module.exports = function(passport) {
-    
+    passport.use(
+        new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+        }, (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+            console.log(profile);
+        })
+    )
 }
